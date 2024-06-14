13bd8e8b9889230c420a7aadcfefbe3e69cbb7a5
Tev Nicolas <tevnicolas@protonmail.com>
2024-06-14 16:05:13 -0700

    Change repo name.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/focus360-todo-list/',
+  base: '/todo-list-challenge/',
   plugins: [react()],
 });
